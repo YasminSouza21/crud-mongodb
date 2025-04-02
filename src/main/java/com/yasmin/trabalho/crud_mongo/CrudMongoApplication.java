81ace1685f44edbcf6464fc4899ee4dd1773b3c2
Yasmin <yasmindesouza090@gmail.com>
2025-04-02 15:07:01 -0300

    add port

--- src/main/java/com/yasmin/trabalho/crud_mongo/CrudMongoApplication.java
+++ src/main/java/com/yasmin/trabalho/crud_mongo/CrudMongoApplication.java
@@ -3,11 +3,15 @@ package com.yasmin.trabalho.crud_mongo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.awt.*;
+import java.net.URI;
+
 @SpringBootApplication
 public class CrudMongoApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(CrudMongoApplication.class, args);
 	}
-
 }
+
